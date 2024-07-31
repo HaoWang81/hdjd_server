d04de239af1bad4828521048a27565fadcd4e3b9
wanghao@staryea.com <fangrongfan_ext@cop.cmsoft.com.cn>
2024-07-31 11:12:16 +0800

    日常提交

--- utils/db.py
+++ utils/db.py
@@ -1,68 +1,75 @@
+import logging
+
 import mysql.connector
+from mysql.connector import pooling
 
 from utils.init import config
 
 
 class MySQLClient:
     def __init__(self, name):
-        self.conn = mysql.connector.connect(
-            host=config[name]['mysql']['host'],
-            user=config[name]['mysql']['username'],
-            password=config[name]['mysql']['password'],
-            database=config[name]['mysql']['database'],
-            autocommit=True,
-            charset="utf8mb4"
-        )
+        pool_config = {
+            'pool_name': 'pocs',
+            'pool_size': 5,  # 连接池大小
+            'pool_reset_session': True,  # 每次从池中获取连接时重置会话状态
+            'host': config[name]['mysql']['host'],
+            # 'port': config_data['mysql']['port'],
+            'database': config[name]['mysql']['database'],
+            'user': config[name]['mysql']['username'],
+            'password': config[name]['mysql']['password'],
+        }
+        try:
+            self.conn = pooling.MySQLConnectionPool(**pool_config)
+        except mysql.connector.Error as err:
+            logging.error(f"Error connecting to MySQL: {err}")
 
     def exec(self, sql, param):
-        if not self.conn.is_connected():
-            self.conn.reconnect()
-        cursor = self.conn.cursor()
+        conn = self.conn.get_connection()
+        cursor = conn.cursor()
         cursor.execute(sql, param)
         cursor.close()
-        self.conn.close()
+        conn.close()
 
     def query(self, sql, param):
-        if not self.conn.is_connected():
-            self.conn.reconnect()
-        cursor = self.conn.cursor()
+        conn = self.conn.get_connection()
+        cursor = conn.cursor()
         cursor.execute(sql, param)
         result = cursor.fetchall()
         cursor.close()
-        self.conn.close()
+        conn.close()
         return result
 
     def delete(self, sql, param):
-        if not self.conn.is_connected():
-            self.conn.reconnect()
-        cursor = self.conn.cursor()
+        conn = self.conn.get_connection()
+        cursor = conn.cursor()
         cursor.execute(sql, param)
         result = cursor.fetchall()
         cursor.close()
-        self.conn.close()
+        conn.close()
 
     def insert_batch(self, sql, param):
-        if not self.conn.is_connected():
-            self.conn.reconnect()
-        cursor = self.conn.cursor()
+        conn = self.conn.get_connection()
+        cursor = conn.cursor()
         try:
-            cursor.executemany(sql, param)
+            for i in range(0, len(param), 5000):
+                batch = param[i:i + 5000]
+                cursor.executemany(sql, batch)
+                # 提交当前批次的更改
+                conn.commit()
+            # cursor.executemany(sql, param)
         except Exception as e:
             print(e)
         cursor.close()
-        self.conn.close()
+        conn.close()
 
     def insert(self, sql, param):
-        if not self.conn.is_connected():
-            self.conn.reconnect()
-        cursor = self.conn.cursor()
+        conn = self.conn.get_connection()
+        cursor = conn.cursor()
         cursor.execute(sql, param)
         cursor.close()
-        self.conn.close()
+        conn.close()
 
     def getWeekDateAreaByCurrent(self, date_str) -> list:
-        if not self.conn.is_connected():
-            self.conn.reconnect()
         from utils.common import currentDateYYYYMMDD
         if date_str is None or len(date_str) == 0:
             date_str = currentDateYYYYMMDD()
@@ -81,8 +88,6 @@ class MySQLClient:
         return result
 
     def getLastWeekDateAreaByCurrent(self, date_str) -> list:
-        if not self.conn.is_connected():
-            self.conn.reconnect()
         from utils.common import currentDateYYYYMMDD
         if date_str is None or len(date_str) == 0:
             date_str = currentDateYYYYMMDD()
@@ -101,4 +106,5 @@ class MySQLClient:
         return result
 
     def close(self):
-        self.conn.close()
+        conn = self.conn.get_connection()
+        conn.close()
